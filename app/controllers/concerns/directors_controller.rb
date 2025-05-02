b127f9e706603cd7d317b171d81a14cf307f57fe
NoFoster <kfoster0@chicagobooth.edu>
2025-05-02 01:15:57 +0000

    Add movie#index and movie

--- app/controllers/concerns/directors_controller.rb
+++ app/controllers/concerns/directors_controller.rb
@@ -1,6 +1,6 @@
 class DirectorsController < ApplicationController
   def index
-      render({ :template => "director_templates/lists"})
+      render({ :template => "director_templates/list"})
   end
 
   def show
