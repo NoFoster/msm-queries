16f94cf2b23af80ec7d979e328791344ce93623a
NoFoster <kfoster0@chicagobooth.edu>
2025-05-02 02:44:59 +0000

    Youngest, eldest, and final commit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -12,4 +12,30 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/details"})
   end
 
+  def youngest
+
+ ordered = Director.where.not({ :dob => nil }).order({ :dob =>:desc })
+
+ the_youngest = ordered.at(0)
+
+    @youngest_id = the_youngest.id
+    @youngest_name = the_youngest.name
+    @youngest_dob = the_youngest.dob.strftime("%B %-d, %Y")
+
+    
+      render({ :template => "director_templates/youngest" })
+  end
+
+  def eldest
+
+    ordered = Director.where.not({ :dob => nil }).order({ :dob =>:asc })
+
+    the_eldest = ordered.at(0)
+   
+       @eldest_id = the_eldest.id
+       @eldest_name = the_eldest.name
+       @eldest_dob = the_eldest.dob.strftime("%B %-d, %Y")
+    render({ :template => "director_templates/eldest" })
+  end
+
 end
